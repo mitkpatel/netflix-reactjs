6f8f8390442e848d64e4e98aea97be5efa0ac751
Mit Patel <mitkpatel09@gmail.com>
2023-02-28 05:50:53 -0500

    SignIn-SignUp-Design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 function App() {
-
   return (
-    <div>
+    <AuthContextProvider>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
-    </div>
+    </AuthContextProvider>
   );
 }
 
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+  return (
+    <div className="w-full h-screen">
+      <img
+        className="hidden sm:block absolute w-full h-full object-cover text-white"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+        alt="bg-signup"
+      />
+      <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+      <div className="fixed w-full px-4 py-24 z-50">
+        <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+          <div className="max-w-[320px] mx-auto py-16">
+            <h1 className="text-3xl font-family">Sign Up</h1>
+            <form className="w-full flex flex-col py-4">
+              <input
+                className="p-3 my-2 bg-gray-700 rounded"
+                type="email"
+                autoComplete="email"
+                placeholder="Email"
+              />
+              <input
+                className="p-3 my-2 bg-gray-700 rounded"
+                type="password"
+                placeholder="Password"
+                autoComplete="current-password"
+              />
+              <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                Sign Up
+              </button>
+              <div className="flex justify-between items-center text-sm text-gray-600">
+                <p>
+                  <input className="mr-2 w-4 h-4" type="checkbox" /> Remember me
+                </p>
+                <p>Need Help?</p>
+              </div>
+              <p className="py-8">
+                <span className="text-gray-600">
+                  Already subscribed to Neflix?
+                </span>{" "}
+                <Link to="/login">Sing In</Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
