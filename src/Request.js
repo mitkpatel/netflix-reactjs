5456158ccbb3051bbbd23ad5cdb80e66bb9c672e
Mit Patel <mitkpatel09@gmail.com>
2023-02-28 03:49:41 -0500

    static-homepage

---
+++ src/Request.js
@@ -0,0 +1,11 @@
+const key = 'e1b98744483efdf5d120f0a9ad1bac0e';
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
---
+++ src/components/Row.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Movie from "./Movie";
+
+const Row = ({ title, fetchURL }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(fetchURL)
+      .then((response) => {
+        setMovies(response.data.results);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }, [fetchURL]);
+
+  console.log("row", movies);
+
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex items-center">
+        <div id={"slider"}>
+          {movies.map((item, id) => (
+            <Movie key={id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Row;
